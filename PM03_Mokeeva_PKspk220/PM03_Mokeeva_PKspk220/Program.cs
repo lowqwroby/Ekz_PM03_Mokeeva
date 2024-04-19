54efd8fe1211a5d57e62969366b79c6fe38563c7
stu-pkspk220 <stu-pkspk220@HQVLA3511BW01>
2024-04-19 08:54:09 +0300

    Создание решения

---
+++ PM03_Mokeeva_PKspk220/PM03_Mokeeva_PKspk220/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PM03_Mokeeva_PKspk220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
